52325a182e028e46b6a05131043860eae5972655
Abhishek bagade <bagdeabhishek@gmail.com>
2017-04-13 13:11:52 +0530

    Final commit integrating all changes

--- VehiSafe/app/src/main/java/com/example/abhishek/vehisafe/MainActivity.java
+++ VehiSafe/app/src/main/java/com/example/abhishek/vehisafe/MainActivity.java
@@ -1,42 +1,93 @@
 package com.example.abhishek.vehisafe;
 
+import android.*;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Arrays;
+
+import static com.example.abhishek.vehisafe.R.id.fab;
 
 //import com.firebase.ui.auth.AuthUI;
 //import com.google.firebase.auth.FirebaseAuth;
 //import com.google.firebase.auth.FirebaseUser;
 
 //import java.util.Arrays;
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    public static final int RC_SIGN_IN =1 ;
+    FragmentActivity fa;
+    public static final int RC_SIGN_IN = 1;
     private FirebaseAuth mAuth;
-    private String AuthUser=null;
+    private String AuthUser = null;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    private FirebaseUser user= null;
-
+    private FirebaseUser user = null;
+    private static Location location = null;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseAuth.AuthStateListener Authlistener;
     @Override
-    protected void onPause() {
-        super.onPause();
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
         mAuth.removeAuthStateListener(mAuthListener);
     }
-    protected void onResume(){
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onResume() {
         super.onResume();
+
+        user=firebaseAuth.getCurrentUser();
         mAuth.addAuthStateListener(mAuthListener);
+
     }
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +96,47 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mAuth = FirebaseAuth.getInstance();
+        final LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//        requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1337);
+        ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        location = locManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if(location==null){
+            location=locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+        if(location==null){
+            location=locManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+        }
+        SupportMapFragment mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         mAuthListener=new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 user=firebaseAuth.getCurrentUser();
             }
              };
+        if(user==null){
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
@@ -60,24 +146,25 @@ public class MainActivity extends AppCompatActivity {
                                 new AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build()))
                         .build(),
                 RC_SIGN_IN);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-
-
-
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        }
+        else{
+        Snackbar.make(findViewById(R.id.content_main), "Welcome user "+user.getDisplayName(), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();}
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Welcome user "+user.getDisplayName(), Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
@@ -89,11 +176,87 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.logout) {
+            firebaseAuth.signOut();
+            Log.d("Clickevent","Event raised");
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+//        Log.d
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()),15));
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(location.getLatitude(), location.getLongitude()))
+
+                .title("You are currently here"));
+
+    }
+}
+class JsonPostRequest extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... params) {
+        try {
+            String address = "http://10.100.115.4:9000/callEmergencyServicePost";
+            JSONObject json = new JSONObject();
+            json.put("nfc_type_id", 1);
+            json.put("nfc_type", 1);
+            json.put("user_id", params[0]);
+            json.put("lattitude", params[1]);
+            json.put("longitude", params[2]);
+            json.put("uid", params[3]);
+//            json.put("email", params[4]);
+
+
+            String requestBody = json.toString();
+            URL url = new URL(address);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            OutputStream outputStream = new BufferedOutputStream(urlConnection.getOutputStream());
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "utf-8"));
+            writer.write(requestBody);
+            writer.flush();
+            writer.close();
+            outputStream.close();
+
+            InputStream inputStream;
+            // get stream
+            if (urlConnection.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
+                inputStream = urlConnection.getInputStream();
+            } else {
+                inputStream = urlConnection.getErrorStream();
+            }
+            // parse stream
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String temp, response = "";
+            while ((temp = bufferedReader.readLine()) != null) {
+                System.out.println(temp);
+                response += temp;
+            }
+            //System.out.println(response);
+            System.out.println(urlConnection.getResponseMessage());
+            // put into JSONObject
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("Content", response);
+            jsonObject.put("Message", urlConnection.getResponseMessage());
+            jsonObject.put("Length", urlConnection.getContentLength());
+            jsonObject.put("Type", urlConnection.getContentType());
+            return jsonObject.toString();
+        } catch (IOException | JSONException e) {
+            return e.toString();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        System.out.println(result);
+        super.onPostExecute(result);
+        Log.d("aaa", "POST RESPONSE: " + result);
+    }
 }
--- VehiSafe/app/src/main/java/com/example/abhishek/vehisafe/NFC_Handler.java
+++ VehiSafe/app/src/main/java/com/example/abhishek/vehisafe/NFC_Handler.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -20,20 +22,26 @@ import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Locale;
 
 public class NFC_Handler extends AppCompatActivity {
 
@@ -46,45 +54,51 @@ public class NFC_Handler extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseUser user=null;
+    private Context context;
+    List<Address> address = null;
+
     @Override
-    protected void onResume(){
-        super.onResume();
+    protected void onStart(){
+        super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
     }
     @Override
-    protected void onPause(){
-        super.onPause();
+    protected void onStop(){
+        super.onStop();
         mAuth.removeAuthStateListener(mAuthListener);
     }
     @RequiresApi(api = Build.VERSION_CODES.M)
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc__handler);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setVisibility(View.INVISIBLE);
-
+        context=this;
         mAuth = FirebaseAuth.getInstance();
         mAuthListener=new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                Log.d("Listener working","Authstate" + firebaseAuth.getCurrentUser().getDisplayName());
                 user=firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    displaySuccess();
+                    sendData();
+                }
+                else{
+                    displayFail();
+                    Intent startIntent=new Intent(getApplicationContext(),MainActivity.class);
+                    getApplicationContext().startActivity(startIntent);
+                }
             }
         };
-         ReadNFCData(this.getIntent());
-        fab.setVisibility(View.VISIBLE);
+        ReadNFCData(this.getIntent());
         getGPS();
-        if(user!=null){
-            displaySuccess();
-            sendData();
-        }
-        else{
-            displayFail();
-            Intent startIntent=new Intent(this,MainActivity.class);
-            this.startActivity(startIntent);
-        }
-        PrintData("Latitude" + location.getLatitude() + "Longitude" + location.getLongitude());
+        sendData();
+        fab.setVisibility(View.VISIBLE);
+        //        PrintData("Latitude" + location.getLatitude() + "Longitude" + location.getLongitude());
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
@@ -127,18 +141,15 @@ public class NFC_Handler extends AppCompatActivity {
     }
 
     private void displayFail() {
-        TextView tv=(TextView)findViewById(R.id.textView);
-        tv.setText("User not Authenticated");
-
+        Log.d("Dispfail", "Failde User= ");
     }
 
-    private void sendData() {
-    }
+
 
     private void displaySuccess() {
-        TextView tv=(TextView)findViewById(R.id.textView);
-        tv.setText("Welcome User "+user);
-
+        Log.d("DIspsuccess","USer "+user.getDisplayName());
+        TextView tv =(TextView)findViewById(R.id.info_text);
+        tv.setText("Welcome user: "+user.getDisplayName()+"\n Emergency Contact Name: "+emergencyName+"\n Emergency phone: "+emergencyPhone);
     }
 
 
@@ -146,7 +157,8 @@ public class NFC_Handler extends AppCompatActivity {
     @RequiresApi(api = Build.VERSION_CODES.M)
     private void getGPS() {
         final LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1337);
+//        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1337);
+        ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION);
         location = locManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -169,14 +181,21 @@ public class NFC_Handler extends AppCompatActivity {
             return;
         }
         location = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(location==null)
+        {
+            location=locManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+        if(location==null){
+            location=locManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+        }
 
         }
 
 
     private void PrintData(String s)
     {
-        Snackbar.make(findViewById(R.id.content_nfc__handler), s, Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+//        Snackbar.make(findViewById(R.id.content_nfc__handler), s, Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show();
         Log.v("LOG Value",s);
     }
     private void ReadNFCData(Intent intent) {
@@ -205,5 +224,14 @@ public class NFC_Handler extends AppCompatActivity {
         emergencyPhone=temp[2];
         PrintData(UID+emergencyName+emergencyPhone);
     }
+    private void sendData() {
+        String Pdata= new String("User: "+emergencyName+"\nEmergency Contact: "+emergencyPhone+"\nUnique ID: "+UID);
+        String latt=""+location.getLatitude();
+        String longtitude=""+location.getLongitude();
+        JsonPostRequest jp= (JsonPostRequest) new JsonPostRequest().execute("1",latt,longtitude,UID);
+        Log.d("SS","sss");
+
+    }
+
 
 }
